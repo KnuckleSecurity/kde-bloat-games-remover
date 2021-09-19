67add12fffa214ce9cc8dd17ea79a956ec5f247f
krygeNNN <burak.baris1@hotmail.com>
2021-09-20 02:21:16 +0300

    Udated -Ss to -Qs

--- game_remover.sh
+++ game_remover.sh
@@ -1,9 +1,7 @@
-sudo pacman -Ss kde games > games.txt &&
+sudo pacman -Qs kde games > games.txt &&
 line_count=$(wc -l games.txt | cut -d " " -f1)
 for ((i=1; i<$line_count; i++));do sed -n $((i++))p games.txt >> remove.txt ;done &&
 line_count_two=$(wc -l remove.txt | cut -d " " -f1) 
 cat remove.txt | cut -d "/" -f2 | cut -d " " -f1 > games.txt ; rm remove.txt &&
 sudo pacman -Rc $(<games.txt) &&
 rm games.txt
-
-
